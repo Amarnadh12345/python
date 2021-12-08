d42d55c609ca670abb10ce1d0588ba3c884f4097
Amarnadh12345 <81435460+Amarnadh12345@users.noreply.github.com>
2021-12-08 16:27:35 +0530

    Add files via upload

---
+++ data.1.py
@@ -0,0 +1,104 @@
+##programes##
+#n=int(input())
+#data=list(map(int,input().split()))
+#os=0
+#es=0
+#for i in data:
+#    if i%2:
+#        os+=i
+#    else:
+#        es+=i
+#print(es,os)        
+
+
+
+
+
+##n=int(input())
+#data=list(map(int,input().split()))
+#dic={0:0,1:0}
+#for i in data:
+#    dic[i%2]+=i
+#for v in dic.values():
+#    print(v,end=" ")
+
+
+
+
+
+
+##n=int(input())
+#data=list(map(int,input().split()))
+#dic={}
+#for i in data:
+#    k=i%2
+#    if k not in dic:
+#       dic[k]=i
+#    else:
+#        dic[k]+=i
+#for v in dic.values():
+#    print(v,end=" ")   
+
+
+
+
+
+
+##n=int(input())
+#data=list(map(int,input().split()))
+#dic={}
+#    if d not in dic:
+#        dic[d]=1
+#    else:
+#        dic[d]+=1
+#for k,v in dic.items():
+#    print(k,v)
+
+
+
+
+
+##n=int(input())
+#data=list(map(int,input().split()))
+#dic={0:0,1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
+#for i in data:
+#    d=i%10
+#    dic[d]+=1
+#for k,v in dic.items():
+#    if v!=0:
+#        print(k,v)
+
+        
+        
+n=int(input())
+data=list(map(int,input().split()))
+dic={}
+for i in data:
+    dic[d]=i
+for k,v in dic.items():
+    print(k,end=" ")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+    
